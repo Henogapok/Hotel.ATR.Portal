e5c590cbd72d9da97dc01942ee3e88667d690889
Henogapok <aprelevvaleriy@gmail.com>
2024-02-10 16:13:04 +0600

    added requests

--- Hotel.ATR.Portal/Controllers/RoomController.cs
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.ATR.Portal.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.ATR.Portal.Controllers
 {
     public class RoomController : Controller
     {
-        public IActionResult Index()
+        private IWebHostEnvironment webHost;
+
+        public RoomController(IWebHostEnvironment webHost)
+        {
+            this.webHost = webHost;
+        }
+
+        public IActionResult Index(int page, int counter)
         {
             return View();
         }
@@ -14,6 +22,26 @@ namespace Hotel.ATR.Portal.Controllers
         }
         public IActionResult RoomDetails()
         {
+            return View();
+        }
+
+        /// <summary>
+        ///1.string email
+        ///2.User user
+        ///3.Request.Form
+        /// </summary>
+        [HttpPost]
+        public IActionResult SubscribeNewsLetter(IFormFile userFile)
+        {
+            var req = Request.Form["email"];
+
+            string path = Path.Combine(webHost.WebRootPath, userFile.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                userFile.CopyTo(stream);
+            }
+
             return View();
         }
     }
